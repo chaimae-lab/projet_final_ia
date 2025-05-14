8e702ff0fb93b6d071b4b4a0f32b0cde9a4521d7
chaimae-lab <chaimae.elinawi@gmail.com>
2025-05-14 19:22:42 +0200

    commit

--- voyage/generat_plan.py
+++ voyage/generat_plan.py
@@ -47,28 +47,3 @@ def plan_voyage(request, critere_id):
 
 
 
-
-#get plan selon id 
-def generer_plan_de_voyage(request, critere_id):
-    critere = get_object_or_404(CritereVoyage, pk=critere_id)
-    prompt = generat_prompt(critere)
-    reponse = obtenir_reponse_deepseek(prompt)
-
-    if reponse:
-        try:
-            # Essayer de parser la réponse en JSON
-            plan_json = json.loads(reponse)
-
-            # Vérification de la structure du plan JSON
-            if "jour1" not in plan_json or "jour2" not in plan_json:
-                return JsonResponse({"erreur": "La réponse JSON ne contient pas le plan de voyage attendu."}, status=400)
-
-            critere.plan_generé = plan_json
-            critere.save()
-            return JsonResponse({"message": "Plan généré avec succès", "plan": plan_json})
-        except json.JSONDecodeError:
-            return JsonResponse({"erreur": "Le contenu reçu n’est pas un JSON valide."}, status=400)
-        except Exception as e:
-            return JsonResponse({"erreur": f"Erreur interne: {str(e)}"}, status=500)
-    else:
-        return JsonResponse({"erreur": "Erreur de génération via l'IA."}, status=500)
